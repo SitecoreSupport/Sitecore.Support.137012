ea55ce5f0aad48040195be07e8c41eeadc020083
Pavel <piv@sitecore.net>
2016-11-29 14:31:54 +0200

    Fix bug #137012: Publish of large blob causes unhandled 'Arithmetic overflow' exception. Fix details: private long GetBlobSize(Guid blobId) method. SQL Query was changed to explicitly cast 'DATALENGTH' type to BIGINT (long)

---
+++ src/Sitecore.Support.137012/Data/SqlServer/SqlServerDataProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Sitecore.Data.DataProviders;
+using Sitecore.Data.DataProviders.SqlServer;
+using Sitecore.Data.SqlServer;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.Data.SqlServer
+{
+  public class SqlServerDataProvider : Sitecore.Data.SqlServer.SqlServerDataProvider
+  {
+    public SqlServerDataProvider(string connectionString) : base(connectionString) {}
+
+    private string ConnectionString
+    {
+      get
+      {
+        var api = Api as SqlServerDataApi;
+        return api?.ConnectionString;
+      }
+    }
+
+    public override Stream GetBlobStream(Guid blobId, CallContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      var blobSize = GetBlobSize(blobId);
+      return blobSize < 0L ? null : OpenBlobStream(blobId, blobSize);
+    }
+
+    private Stream OpenBlobStream(Guid blobId, long blobSize)
+    {
+      const string cmdText = "SELECT [Data]\r\n                     FROM [Blobs]\r\n                     WHERE [BlobId] = @blobId\r\n                     ORDER BY [Index]";
+      var connection = OpenConnection();
+      try
+      {
+        var command = new SqlCommand(cmdText, connection)
+        {
+          CommandTimeout = (int) CommandTimeout.TotalSeconds
+        };
+        command.Parameters.AddWithValue("@blobId", blobId);
+        var reader = command.ExecuteReader(CommandBehavior.CloseConnection | CommandBehavior.SequentialAccess);
+        try
+        {
+          return new SqlServerStream(reader, blobSize);
+        }
+        catch (Exception exception)
+        {
+          reader.Close();
+          Log.Error("Error reading blob stream (blob id: " + blobId + ")", exception, this);
+        }
+      }
+      catch (Exception exception2)
+      {
+        connection.Close();
+        Log.Error("Error reading blob stream (blob id: " + blobId + ")", exception2, this);
+      }
+      return null;
+    }
+
+
+    private long GetBlobSize(Guid blobId)
+    {
+      const string cmdText = " SELECT SUM(CAST(DATALENGTH([Data]) AS BIGINT)) FROM [Blobs] WHERE [BlobId] = @blobId";
+      using (var connection = OpenConnection())
+      {
+        var command = new SqlCommand(cmdText, connection)
+        {
+          CommandTimeout = (int) CommandTimeout.TotalSeconds
+        };
+        command.Parameters.AddWithValue("@blobId", blobId);
+        using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+        {
+          if (!reader.Read()) return -1L;
+          if (reader.IsDBNull(0)) return -1L;
+          return SqlServerHelper.GetLong(reader, 0);
+        }
+      }
+    }
+
+    private SqlConnection OpenConnection()
+    {
+      var connection = new SqlConnection(ConnectionString);
+      connection.Open();
+      return connection;
+    }
+  }
+}
---
+++ src/Sitecore.Support.137012/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.137012")]
+[assembly: AssemblyProduct("Sitecore.Support.137012")]
+[assembly: ComVisible(false)]
